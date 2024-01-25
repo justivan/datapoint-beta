925c3695be7b89a655be17eed257389fbce7c688
justivan <justivan.dev@gmail.com>
2024-01-25 14:21:33 +0000

    updated gwg migration

--- app/gwg/migrations/0001_initial.py
+++ app/gwg/migrations/0001_initial.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name="Booking",
+            name="Reservation",
             fields=[
                 ("ref_id", models.IntegerField(primary_key=True, serialize=False)),
                 ("res_id", models.IntegerField()),
