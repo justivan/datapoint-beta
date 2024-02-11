81ea60c6d72a16825beee7fceb9a2b716e5228e5
justivan <justivan.dev@gmail.com>
2024-02-11 00:51:48 +0000

    updated views

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 from .views import (
-    UserListAPIView,
-    CountryListAPIView,
-    RegionListAPIView,
-    AreaListAPIView,
+    user_list,
+    country_list,
+    region_list,
+    area_list,
 )
 
 app_name = "api"
 urlpatterns = [
-    path("user/", UserListAPIView.as_view(), name="user-list"),
-    path("country/", CountryListAPIView.as_view(), name="country-list"),
-    path("region/", RegionListAPIView.as_view(), name="region-list"),
-    path("area/", AreaListAPIView.as_view(), name="area-list"),
+    path("user/", user_list, name="user-list"),
+    path("country/", country_list, name="country-list"),
+    path("region/", region_list, name="region-list"),
+    path("area/", area_list, name="area-list"),
 ]
--- app/api/views.py
+++ app/api/views.py
@@ -1,5 +1,6 @@
-from rest_framework.generics import ListAPIView
+from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from django.contrib.gis.geos import Point
 from django.contrib.auth import get_user_model
 
@@ -10,32 +11,47 @@ from users.serializers import UserSerializer
 User = get_user_model()
 
 
-class UserListAPIView(ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+@api_view(["GET", "POST"])
+def user_list(request):
+    if request.method == "GET":
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    elif request.method == "POST":
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class CountryListAPIView(ListAPIView):
-    queryset = Country.objects.all()
-    serializer_class = CountrySerializer
+@api_view(["GET"])
+def country_list(request):
+    countries = Country.objects.all()
+    serializer = CountrySerializer(countries, many=True)
+
+    return Response(serializer.data)
 
 
-class RegionListAPIView(ListAPIView):
-    queryset = Region.objects.all()
-    serializer_class = RegionSerializer
+@api_view(["GET"])
+def region_list(request):
+    regions = Region.objects.all()
+    serializer = RegionSerializer(regions, many=True)
+
+    return Response(serializer.data)
 
 
-class AreaListAPIView(ListAPIView):
-    serializer_class = AreaSerializer
+@api_view(["GET"])
+def area_list(request):
+    lat = request.query_params.get("lat")
+    lng = request.query_params.get("lng")
 
-    def get_queryset(self):
-        lat = self.request.query_params.get("lat")
-        lng = self.request.query_params.get("lng")
+    if lat is not None and lng is not None:
+        point = Point(float(lng), float(lat), srid=4326)
+        queryset = Area.objects.filter(geom__contains=point)
+    else:
+        queryset = Area.objects.all()
 
-        if lat is not None and lng is not None:
-            point = Point(float(lng), float(lat), srid=4326)
-            queryset = Area.objects.filter(geom__contains=point)
-        else:
-            queryset = Area.objects.all()
-
-        return queryset
+    serializer = AreaSerializer(queryset, many=True)
+    return Response(serializer.data)
